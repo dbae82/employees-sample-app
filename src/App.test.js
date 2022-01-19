12745f71bdf40c1d48c4e2c97f002afe52885221
dbae82 <dbae82@gmail.com>
2022-01-18 20:30:49 -0600

    edit: code cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Employees in h1 tag in DOM', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Employees/i);
+  expect(element).toBeInTheDocument();
 });
